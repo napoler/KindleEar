4174315c0740c7963cc74481744315a00a9509b1
napoler <napoler2008@gmail.com>
2020-10-22 11:07:44 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 python helper.py
-appcfg.py update ./app.yaml ./module-worker.yaml
-appcfg.py update .
+appcfg.py update ./app.yaml ./module-worker.yaml --noauth_local_webserver
+appcfg.py update . --noauth_local_webserver
