2a54358f493c6ea562791e6ca571a64d3ae521e0
napoler <napoler2008@gmail.com>
2020-10-27 10:45:30 +0800

    Update upload.sh
    修改为gcloud提交

--- upload.sh
+++ upload.sh
@@ -1,3 +1,7 @@
-python helper.py
-appcfg.py update ./app.yaml ./module-worker.yaml --noauth_local_webserver
-appcfg.py update . --noauth_local_webserver
+#python helper.py
+#appcfg.py update ./app.yaml ./module-worker.yaml --noauth_local_webserver
+#appcfg.py update . --noauth_local_webserver
+gcloud auth login
+gcloud config set project terry-kindle
+gcloud app deploy --version=1 ./app.yaml ./module-worker.yaml
+gcloud app deploy --version=1 ./
